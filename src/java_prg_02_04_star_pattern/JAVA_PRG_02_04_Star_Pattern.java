6c2c7ca8d44906797aee0a7c8dd801d013d1ab17
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-13 18:37:17 -0400

    Initial version tested and working.

---
+++ src/java_prg_02_04_star_pattern/JAVA_PRG_02_04_Star_Pattern.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_02_04_star_pattern;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that displays the following pattern:
+ *    *
+ *   ***
+ *  *****
+ * *******
+ *  *****
+ *   ***
+ *    *
+ */
+public class JAVA_PRG_02_04_Star_Pattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("   *\n");
+        System.out.print("  ***\n");
+        System.out.print(" *****\n");
+        System.out.print("*******\n");
+        System.out.print(" *****\n");
+        System.out.print("  ***\n");
+        System.out.println("   *");
+    }
+    
+}
